01a10e5a4d18eecbd991a002790b0eb32c39061e
Carl Burnham <cburnham@letshang.us>
2016-08-03 15:18:52 -0400

    Added location Listener

---
+++ app/src/main/java/letshangllc/letsride/LocationTracker.java
@@ -0,0 +1,159 @@
+package letshangllc.letsride;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Carl on 8/3/2016.
+ */
+public class LocationTracker implements LocationListener {
+
+    /* Calling Activity */
+    private final AppCompatActivity appCompatActivity;
+
+    // flag for GPS Status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    android.location.Location location;
+    double latitude;
+    double longitude;
+
+    // The minimum distance to change updates in metters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = (long) 0.5;
+
+    // The minimum time beetwen updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 2000;
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    public LocationTracker(AppCompatActivity activity) {
+        this.appCompatActivity = activity;
+        getLocation();
+    }
+
+    public android.location.Location getLocation() {
+        try {
+            locationManager = (LocationManager) appCompatActivity
+                    .getSystemService(AppCompatActivity.LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // location service disabled
+                Log.i("GPS: ", "Disabled");
+                Toast.makeText(appCompatActivity, "Enable Location Service", Toast.LENGTH_LONG).show();
+            } else {
+                this.canGetLocation = true;
+
+                // if GPS Enabled get lat/long using GPS Services
+
+                /* Request permissions */
+                if (ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(appCompatActivity, new String[]{
+                            Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+                    }, 0);
+
+                }
+
+                if (isGPSEnabled) {
+
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    Log.d("GPS Enabled", "GPS Enabled");
+
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        //updateGPSCoordinates();
+                    }
+                }
+
+                // Next get location from Network Provider if no previous location
+                if (isNetworkEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                        Log.d("Network", "Network");
+
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            //updateGPSCoordinates();
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            Log.e("Error : Location",
+                    "Impossible to connect to LocationManager", e);
+        }
+
+        return location;
+    }
+
+    public void updateGPSCoordinates(Location location) {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    public void onLocationChanged(Location location) {
+        //Toast.makeText(getApplicationContext(), : " + location.toString(), Toast.LENGTH_LONG).show();
+        updateGPSCoordinates(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    public void onProviderDisabled(String provider) {
+    }
+
+    public void onProviderEnabled(String provider) {
+    }
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- app/src/main/java/letshangllc/letsride/MainActivity.java
+++ app/src/main/java/letshangllc/letsride/MainActivity.java
@@ -1,13 +1,132 @@
 package letshangllc.letsride;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private final static int REQUEST_LOCATION_PERMISSIONS = 0;
+
+    // The minimum distance to change updates in metters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = (long) 0.5;
+
+    // The minimum time beetwen updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 2000;
+
+    /* VIEWS */
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        requestPermission();
+    }
+
+    private void requestPermission() {
+        /* If permissions not allowed then request them */
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+            }, REQUEST_LOCATION_PERMISSIONS);
+
+        } else {
+            startLocationListener();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (REQUEST_LOCATION_PERMISSIONS == requestCode) {
+            startLocationListener();
+        }
+    }
+
+    private void startLocationListener() {
+        try {
+            LocationManager locationManager = (LocationManager) this
+                    .getSystemService(AppCompatActivity.LOCATION_SERVICE);
+
+            // getting GPS status
+            boolean isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            boolean isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            /* If both are disabled then request location be enabled */
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // location service disabled
+                Log.i("GPS: ", "Disabled");
+                Toast.makeText(this, "Enable Location Service", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            } else {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {}
+
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    Log.d("Network", "Network");
+                }else if (isGPSEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                }
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Error : Location",
+                    "Impossible to connect to LocationManager", e);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "Location Updated");
+        if(location!= null){
+
+        }
+    }
+
+    private void findViews(){
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
