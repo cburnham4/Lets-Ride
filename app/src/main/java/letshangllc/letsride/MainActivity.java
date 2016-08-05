461aad98b0620e4fd7efb60c1385e9006adbc574
Carl Burnham <cburnham@letshang.us>
2016-08-05 15:57:41 -0400

    REquest permissions and location services upon opening app

--- app/src/main/java/letshangllc/letsride/MainActivity.java
+++ app/src/main/java/letshangllc/letsride/MainActivity.java
@@ -24,7 +24,10 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    /* Permission Variables */
     private final static int REQUEST_LOCATION_PERMISSIONS = 0;
+    private boolean locationPermissionsEnabled;
 
     // The minimum distance to change updates in metters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = (long) 0.5;
@@ -45,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     /* Location Variables */
     private LocationManager locationManager;
+    private boolean locationEnabled = false;
 
     /* VIEWS */
     private TextView tvCurrentSpeed, tvMaxSpeed, tvAvgSpeed, tvCurrentElevation, tvMaxElevation,
@@ -57,35 +61,65 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_main);
 
         findViews();
+        this.requestPermission();
+        this.requestLocationEnabled();
     }
 
-    private void requestPermissionAndStart() {
+    /* Make sure the user has location permissions enabled */
+    private void requestPermission() {
         /* If permissions not allowed then request them */
+        Log.i(TAG, "Reqeuest Permission");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.enable_location_service), Toast.LENGTH_LONG).show();
+            Log.i(TAG, "Permission is requested");
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
             }, REQUEST_LOCATION_PERMISSIONS);
 
         } else {
-            startLocationListener();
+            Log.i(TAG, "Location Permissions are enabled ");
+            locationPermissionsEnabled = true;
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.i(TAG, "Permission Request Result");
         if (REQUEST_LOCATION_PERMISSIONS == requestCode) {
-            startLocationListener();
+            locationPermissionsEnabled =true;
         }
     }
 
-    /* TODO: Create Enable location method */
+    /* Request that location services are enabled */
+    private void requestLocationEnabled(){
+        locationManager = (LocationManager) this
+                .getSystemService(AppCompatActivity.LOCATION_SERVICE);
+
+        // getting GPS status
+        boolean isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        boolean isNetworkEnabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            /* If both are disabled then request location be enabled */
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            // location service disabled
+            Log.i("GPS: ", "Disabled");
+            Toast.makeText(this, getString(R.string.enable_location_service), Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        } else{
+            locationEnabled = true;
+        }
+    }
+
     private void startLocationListener() {
+        Log.i(TAG, "Start Location Listener");
         isRecording = true;
         try {
-            locationManager = (LocationManager) this
-                    .getSystemService(AppCompatActivity.LOCATION_SERVICE);
-
             // getting GPS status
             boolean isGPSEnabled = locationManager
                     .isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -102,32 +136,20 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(intent);
             } else {
+                Log.i(TAG, "Location services are enabled");
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 }
 
-                if (isNetworkEnabled && isGPSEnabled) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
+                if (isGPSEnabled) {
                     locationManager.requestLocationUpdates(
                             LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
-                    startTimer();
                 } else if (isNetworkEnabled) {
                     locationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    startTimer();
-                } else if(isGPSEnabled){
-                    locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-                    startTimer();
                 } else{
                     Toast.makeText(this, "Enable Location Services", Toast.LENGTH_LONG).show();
                 }
@@ -141,23 +163,35 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
     }
 
-    private void startTimer(){
-        /* Start the timer */
-        stopWatch.start();
-        /* Start the handler to request times from stopwatch */
-        handler.post(updateTimer);
+    private void startRecording(){
+        Log.i(TAG, "Start Timer");
+        if(locationEnabled && locationPermissionsEnabled){
+            startLocationListener();
+            /* Start the timer */
+            stopWatch.start();
+            /* Start the handler to request times from stopwatch */
+            handler.post(updateTimer);
+        } else{
+            if(!locationEnabled){
+                requestLocationEnabled();
+            }
+            if(!locationPermissionsEnabled){
+                requestPermission();
+            }
+        }
     }
 
     private void stopRecording() {
+        Log.i(TAG, "Stop Recording ");
         isRecording = false;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Enbable Location Services", Toast.LENGTH_LONG).show();
             return;
         }
-        tvDuration.setText(getString(R.string.time_zero));
+        resetViewsAndData();
         locationManager.removeUpdates(this);
+
         stopWatch.stop();
-        speed.speeds.clear();
         handler.removeCallbacks(updateTimer);
     }
 
@@ -167,6 +201,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         stopWatch.pause();
     }
 
+    private void resetViewsAndData(){
+        tvDuration.setText(getString(R.string.time_zero));
+        tvCurrentSpeed.setText(getString(R.string.empty));
+        tvMaxSpeed.setText(getString(R.string.empty));
+        tvAvgSpeed.setText(getString(R.string.empty));
+        tvMinElevation.setText(getString(R.string.empty));
+        tvMaxElevation.setText(getString(R.string.empty));
+        tvCurrentElevation.setText(getString(R.string.empty));
+
+        speed.speeds.clear();
+    }
     @Override
     public void onLocationChanged(Location location) {
         Log.i(TAG, "Location Updated");
@@ -221,7 +266,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 if(!isRecording){
                     fabStartPauseRecording.setImageResource(R.drawable.ic_pause_white_48dp);
                     fabStopRecording.setVisibility(View.GONE);
-                    requestPermissionAndStart();
+                    startRecording();
                     /* If it is recording the pause it */
                 }else{
                     fabStartPauseRecording.setImageResource(R.drawable.ic_play_arrow_white_48dp);
@@ -243,7 +288,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     public Runnable updateTimer = new Runnable() {
         public void run() {
-
             /* Remove callbacks on stop */
             //handler.removeCallbacks(updateTimer);
             if(isRecording){
@@ -256,8 +300,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 /* Run updateTimte again in 100ms */
                 handler.postDelayed(this, 300);
             }
-
-
         }};
 
     /* Unused Location Listener functions */
